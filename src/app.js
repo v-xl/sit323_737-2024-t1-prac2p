6022843b51aeb2af387693529348e5297ba4f221
Matt <140072961+v-xl@users.noreply.github.com>
2024-03-18 20:07:01 +1100

    second commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
+//We require the 'express' library, which is the webserver
 const express = require("express");
 const res = require("express/lib/response");
 const app = express();
 
+//Adds two numbers together and returns the result
 const addTwoNumber = (n1, n2) => {
     return n1+n2;
 }
 
+//serving data on the page /addTwoNumber
+//This will also run the addTwoNumber function, and return the result (if any)
 app.get("/addTwoNumber", (req, res) => {
     const n1 = parseInt(req.query.n1);
     const n2 = parseInt(req.query.n2);
@@ -13,13 +17,16 @@ app.get("/addTwoNumber", (req, res) => {
     res.json({statuscode:200, data: result});
 });
 
+//Serving data at root of the website
 app.get('/', function (req, res){
     res.send('hello world');
 });
 
+//Do a test of the addTwoNumber function
 console.log(addTwoNumber(9, 22));
 const port = 3000;
 
+//Start listening on the specified port.
 app.listen(port, () => {
     console.log("Hello! I'm now listening on port: " + port);
 });
